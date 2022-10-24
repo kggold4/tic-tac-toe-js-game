3c0c16d3a77872625eda539d9bde93fa667e09a8
Kfir Goldfarb <kggold4@gmail.com>
2022-10-24 19:53:12 +0300

    Add testRandomPlay function (#2) for test a random play of the game

---
+++ test/test_functions.js
@@ -0,0 +1,19 @@
+function getEmptyCells() {
+    let empty_cells = [];
+    let current_board = document.getElementById("board");
+    let game_cells = current_board.childNodes;
+    for(let game_cell of game_cells) {
+        if(!game_cell.innerHTML) {
+            empty_cells.push(game_cell);
+        }
+    }
+    return empty_cells;
+}
+
+function testRandomPlay() {
+    while(!done) {
+        let cells_to_click = getEmptyCells();
+        let cell_to_click = cells_to_click[Math.floor(Math.random() * cells_to_click.length)];
+        cell_to_click.click();
+    }
+}
